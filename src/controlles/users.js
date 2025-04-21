31d3e3bdfedff3b23e8c7813f012ba6a82404125
DaminboevM <m701rizo@gmail.com>
2025-04-21 21:59:03 +0500

    Hamma ozini ismini tagidagi fuksiyani yozsin !

--- src/controlles/users.js
+++ src/controlles/users.js
@@ -4,24 +4,30 @@ import path from "path"
 const students = JSON.parse(fs.readFileSync(path.join(process.cwd(), "database/users.json")))
 
 
+
+// Abrorbe 
 const GET = (req, res) => {
     console.log(students)
 }
 
 
 
+// Aborbek
 const GET_ID = (req, res) => {}
 
 
 
+// Abdulloh
 const POST = (req, res) => {}
 
 
 
+// Muhammadrizo
 const PUT = (req, res) => {}
 
 
 
+// Ozodek
 const DELETE = (req, res) => {}
 
 
